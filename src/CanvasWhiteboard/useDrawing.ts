d1759fb264651754ccdd843da2bf133c121e51f1
vuchint sutapalli <vuchint.sutapalli@gmail.com>
2025-09-15 02:39:51 +0530

    updated rotate handle

--- src/CanvasWhiteboard/useDrawing.ts
+++ src/CanvasWhiteboard/useDrawing.ts
@@ -384,7 +384,7 @@ export const useDrawing = ({
 		};
 
 		const rotIcon = new Image();
-		const rotationIconSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#0000ff" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M21.5 2v6h-6M2.5 22v-6h6"/><path d="M22 11.5A10 10 0 0 0 3.5 12.5"/><path d="M2 12.5a10 10 0 0 0 18.5-1"/></svg>`;
+		const rotationIconSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#0000ff" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="23 4 23 10 17 10"></polyline><path d="M20.49 15a9 9 0 1 1-2.12-9.36L23 10"></path></svg>`;
 		rotIcon.src = `data:image/svg+xml;utf8,${encodeURIComponent(rotationIconSvg)}`;
 		rotIcon.onload = () => {
 			rotationIconRef.current = rotIcon;
